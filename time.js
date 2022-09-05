85833b7fec884197306b98d4a374a5a78dfbb432
Karinny Lemos <99560287+karinnylemosb@users.noreply.github.com>
2022-09-05 14:32:19 -0300

    first commit

---
+++ time.js
@@ -0,0 +1,13 @@
+const timeDoLeo = [
+  'Vitoria',
+  'Ariana',
+  'Isvilly',
+  'Liriel',
+  'Andreza',
+  'Clara',
+  'Kemely',
+];
+
+const randomizaTime = timeDoLeo[Math.floor(Math.random() * timeDoLeo.length)];
+
+console.log('A vencedora da vez foi:  ', randomizaTime);
